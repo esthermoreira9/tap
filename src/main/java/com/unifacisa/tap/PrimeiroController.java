35006f2986045ae409417ddbe1ad3b6e5d30c587
esthermoreira9 <esther.moreira@maisunifacisa.com.br>
2023-08-23 22:33:22 -0300

    Fixing endpoint

--- src/main/java/com/unifacisa/tap/PrimeiroController.java
+++ src/main/java/com/unifacisa/tap/PrimeiroController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import model.Pessoa;
 
 @RestController
-@RequestMapping("api/produtos")
+@RequestMapping("/api")
 public class PrimeiroController {
 
 	private List<Pessoa> pessoas = new ArrayList<>();
